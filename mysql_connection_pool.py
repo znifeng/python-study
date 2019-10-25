700730b5c910fc80b1c379c2a65f2702da4d971e
大倪 <zhifeng.nzf@alibaba-inc.com>
2019-10-25 17:44:25 +0800

    update

--- mysql_connection_pool.py
+++ mysql_connection_pool.py
@@ -1,28 +1,29 @@
-
 # -*- coding: UTF-8 -*-
 """
 1、执行带参数的ＳＱＬ时，请先用sql语句指定需要输入的条件列表，然后再用tuple/list进行条件批配
 ２、在格式ＳＱＬ中不需要使用引号指定数据类型，系统会根据输入参数自动识别
 ３、在输入的值中不需要使用转意函数，系统会自动处理
 """
- 
+
 import MySQLdb
 from MySQLdb.cursors import DictCursor
 from DBUtils.PooledDB import PooledDB
-import datetime
- 
+
 """
 Config是一些数据库的配置文件
 """
 
+
 class Config(object):
-    DBHOST='localhost'
-    DBPORT=3306
-    DBUSER='root'
-    DBPWD='root'
-    DBNAME='smart_test'
-    DBCHAR='utf8'
-    POOL=None
+    DBHOST = 'localhost'
+    DBPORT = 3306
+    DBUSER = 'root'
+    DBPWD = 'root'
+    DBNAME = 'smart_test'
+    DBCHAR = 'utf8'
+    POOL = PooledDB(creator=MySQLdb, mincached=1, maxcached=20,
+                    host=DBHOST, port=DBPORT, user=DBUSER, passwd=DBPWD,
+                    db=DBNAME, use_unicode=False, charset=DBCHAR, cursorclass=DictCursor)
 
 
 class Mysql(object):
@@ -30,28 +31,19 @@ class Mysql(object):
     MYSQL数据库对象，负责产生数据库连接 , 此类中的连接采用连接池实现获取连接对象：conn = Mysql.getConn()
             释放连接对象;conn.close()或del conn
     """
-    #连接池对象
-    
-    def __init__(self):
-        #数据库构造函数，从连接池中取出连接，并生成操作游标
-        self._conn = Mysql.__getConn()
+
+    # 连接池对象
+
+    def __init__(self, config):
+        # 数据库构造函数，从连接池中取出连接，并生成操作游标
+        self._config = config
+        self._conn = self.getConn()
         self._cursor = self._conn.cursor()
-    
-    @staticmethod
-    def __getConn():
-        """
-        @summary: 静态方法，从连接池中取出连接
-        @return MySQLdb.connection
-        """
-        if Config.POOL is None:
-            Config.POOL = PooledDB(creator=MySQLdb, mincached=1 , maxcached=20 ,
-                              host=Config.DBHOST , port=Config.DBPORT , user=Config.DBUSER , passwd=Config.DBPWD ,
-                              db=Config.DBNAME,use_unicode=False,charset=Config.DBCHAR,cursorclass=DictCursor)
-        # else:
-        #     print "pool is not none"
-        return Config.POOL.connection()
- 
-    def getAll(self,sql,param=None):
+
+    def getConn(self):
+        return self._config.POOL.connection()
+
+    def getAll(self, sql, param=None):
         """
         @summary: 执行查询，并取出所有结果集
         @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
@@ -61,14 +53,14 @@ class Mysql(object):
         if param is None:
             count = self._cursor.execute(sql)
         else:
-            count = self._cursor.execute(sql,param)
-        if count>0:
+            count = self._cursor.execute(sql, param)
+        if count > 0:
             result = self._cursor.fetchall()
         else:
             result = False
         return result
- 
-    def getOne(self,sql,param=None):
+
+    def getOne(self, sql, param=None):
         """
         @summary: 执行查询，并取出第一条
         @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
@@ -78,14 +70,14 @@ class Mysql(object):
         if param is None:
             count = self._cursor.execute(sql)
         else:
-            count = self._cursor.execute(sql,param)
-        if count>0:
+            count = self._cursor.execute(sql, param)
+        if count > 0:
             result = self._cursor.fetchone()
         else:
             result = False
         return result
- 
-    def getMany(self,sql,num,param=None):
+
+    def getMany(self, sql, num, param=None):
         """
         @summary: 执行查询，并取出num条结果
         @param sql:查询ＳＱＬ，如果有查询条件，请只指定条件列表，并将条件值使用参数[param]传递进来
@@ -96,33 +88,33 @@ class Mysql(object):
         if param is None:
             count = self._cursor.execute(sql)
         else:
-            count = self._cursor.execute(sql,param)
-        if count>0:
+            count = self._cursor.execute(sql, param)
+        if count > 0:
             result = self._cursor.fetchmany(num)
         else:
             result = False
         return result
- 
-    def insertOne(self,sql,value):
+
+    def insertOne(self, sql, value):
         """
         @summary: 向数据表插入一条记录
         @param sql:要插入的ＳＱＬ格式
         @param value:要插入的记录数据tuple/list
         @return: insertId 受影响的行数
         """
-        self._cursor.execute(sql,value)
+        self._cursor.execute(sql, value)
         return self.__getInsertId()
- 
-    def insertMany(self,sql,values):
+
+    def insertMany(self, sql, values):
         """
         @summary: 向数据表插入多条记录
         @param sql:要插入的ＳＱＬ格式
         @param values:要插入的记录数据tuple(tuple)/list[list]
         @return: count 受影响的行数
         """
-        count = self._cursor.executemany(sql,values)
+        count = self._cursor.executemany(sql, values)
         return count
- 
+
     def __getInsertId(self):
         """
         获取当前连接最后一次插入操作生成的id,如果没有则为０
@@ -130,61 +122,59 @@ class Mysql(object):
         self._cursor.execute("SELECT @@IDENTITY AS id")
         result = self._cursor.fetchall()
         return result[0]['id']
- 
-    def __query(self,sql,param=None):
+
+    def __query(self, sql, param=None):
         if param is None:
             count = self._cursor.execute(sql)
         else:
-            count = self._cursor.execute(sql,param)
+            count = self._cursor.execute(sql, param)
         return count
- 
-    def update(self,sql,param=None):
+
+    def update(self, sql, param=None):
         """
         @summary: 更新数据表记录
         @param sql: ＳＱＬ格式及条件，使用(%s,%s)
         @param param: 要更新的  值 tuple/list
         @return: count 受影响的行数
         """
-        return self.__query(sql,param)
- 
-    def delete(self,sql,param=None):
+        return self.__query(sql, param)
+
+    def delete(self, sql, param=None):
         """
         @summary: 删除数据表记录
         @param sql: ＳＱＬ格式及条件，使用(%s,%s)
         @param param: 要删除的条件 值 tuple/list
         @return: count 受影响的行数
         """
-        return self.__query(sql,param)
- 
+        return self.__query(sql, param)
+
     def begin(self):
         """
         @summary: 开启事务
         """
         self._conn.autocommit(0)
- 
-    def end(self,option='commit'):
+
+    def end(self, option='commit'):
         """
         @summary: 结束事务
         """
-        if option=='commit':
+        if option == 'commit':
             self._conn.commit()
         else:
             self._conn.rollback()
-    
-    def dispose(self,isEnd=1):
+
+    def dispose(self, isEnd=1):
         """
         @summary: 释放连接池资源
         """
-        if isEnd==1:
+        if isEnd == 1:
             self.end('commit')
         else:
             self.end('rollback');
         self._cursor.close()
         self._conn.close()
 
-if __name__ == "__main__":
-    mysql = Mysql()
-    ##服务器时区为UTC，转为当前时区+8小时
-    dt=(datetime.datetime.now() + datetime.timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
-    mysql.insertOne("insert into tbl_evaluate_tasks(gmt_create, gmt_modified, api, params, result) values(%s,%s,%s,%s,%s)", [dt, dt, api, params, result])
-    mysql.dispose()
+if __name__ == '__main__':
+    mysql = Mysql(Config)
+    result= mysql.getOne("select * from tbl_evaluate_tasks limit 1")
+    print result
